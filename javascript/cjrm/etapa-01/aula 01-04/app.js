2344ce0c51dd0d01b8752a4b99b83d4fa2eb2f60
Raíza Cirne <raizacirne@gmail.com>
2025-03-05 00:42:22 -0300

    chore: organize repository folders

---
+++ javascript/cjrm/etapa-01/aula 01-04/app.js
@@ -0,0 +1,20 @@
+/*
+comentário multilinha
+*/
+
+// comentário 
+
+let age = 31;
+let currentYear = 2019;
+
+console.log(age, currentYear);
+
+age = 32;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+// No JavaScript moderno não se utiliza mais var 
+var score = 75;
+console.log(score);
