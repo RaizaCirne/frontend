45763917d1e46fe11c24ae568267f411af3c04b1
Raíza Cirne <raizacirne@gmail.com>
2025-03-08 17:49:24 -0300

    feat: add numbers

---
+++ javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-02-05-numbers/app.js
@@ -0,0 +1,43 @@
+// inteiros e decimeais
+const radius = 10;
+const pi = 3.14;
+
+// operadores aritméticos
+const area = pi * radius ** 2;
+
+/*
+console.log(10 / 2);
+const reminder = 5 % 2;
+const reminder = 4 % 2;
+console.log(reminder);
+*/
+
+// ordem de operações
+const crazyOperation = 5 * (10 - 3) ** 2;
+
+// 1º parênteses
+// 2º expoentes ou raízes
+// 3º multiplicaão e divisão
+// 4º adicção e subtração
+
+// operadores de incremento e decremento
+let postLikes = 10;
+// postLikes = postLikes + 1;
+// postLikes++;
+// postLikes--;
+
+// operadores addition, subtraction, multiplication e division assignment
+// postLikes = postLikes + 10;
+// postLikes += 10;
+// postLikes -= 5;
+// postLikes *= 3;
+// postLikes /= 2;
+// console.log(postLikes);
+
+// NaN - not a number
+console.log(7 / "oi");
+
+// contatenação de string com número
+const likesMessage = "O post tem " + postLikes + " likes.";
+
+console.log(likesMessage);
