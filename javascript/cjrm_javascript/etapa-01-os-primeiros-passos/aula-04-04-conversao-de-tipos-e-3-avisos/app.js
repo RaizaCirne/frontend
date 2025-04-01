38afb8cf7a3ede683801a928e03bb4b22f25e1ca
Raíza Cirne <raizacirne@gmail.com>
2025-04-01 07:06:07 -0300

    docs: add JavaScript type conversion examples

---
+++ javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-04-04-conversao-de-tipos-e-3-avisos/app.js
@@ -0,0 +1,30 @@
+// Conversão de tipos
+let score = "100";
+
+// score = Number(score);
+
+// console.log(score + 1);
+// console.log(typeof score);
+
+const crazyConversion = Number("Maçã");
+const convertNumber = String(97);
+const booleanConversion = Boolean(10);
+
+// console.log(crazyConversion);
+// console.log(convertNumber, typeof convertNumber);
+// console.log(booleanConversion, typeof booleanConversion);
+// console.log(Boolean(0));
+console.log(Boolean("0"));
+
+/*
+Valores falsy: 
+  - false
+  - 0
+  - "", '', ``
+  - null
+  - undefined
+  - NaN 
+
+Valores truthy:
+  - Qualquer valor que não é falsy
+*/
