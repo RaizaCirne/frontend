2a99014df5e295abfce9548bbd8d644f3c3a8977
Raíza Cirne <raizacirne@gmail.com>
2025-04-12 17:36:00 -0300

    docs(js): add console.log notes and basic execution flow test

--- javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-01-03-o-console-do-browser/app.js
+++ javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-01-03-o-console-do-browser/app.js
@@ -1,2 +1,8 @@
 console.log(1);
 console.log(2);
+
+// Esse código vai ser executado de forma sequencial, porque o JavaScript é executado de cima para baixo.
+
+// A primeira linha é executada, e em seguida, a segunda linha. 
+
+// console.log() é um MÉTODO que podemos usar para exibir valores no console do DevTools
