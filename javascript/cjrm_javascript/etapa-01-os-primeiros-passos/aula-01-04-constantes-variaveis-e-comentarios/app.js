e9d2792d14ab2031d8fe2075f940e7bad73b03c1
Raíza Cirne <raizacirne@gmail.com>
2025-04-12 17:49:40 -0300

    docs(js): add detailed comments about variables, naming and best practices

--- javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-01-04-constantes-variaveis-e-comentarios/app.js
+++ javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-01-04-constantes-variaveis-e-comentarios/app.js
@@ -1,9 +1,3 @@
-/*
-comentário multilinha
-*/
-
-// comentário 
-
 let age = 31;
 let currentYear = 2019;
 
@@ -18,3 +12,31 @@ console.log(points);
 // No JavaScript moderno não se utiliza mais var 
 var score = 75;
 console.log(score);
+
+
+/*
+Variáveis em JavaScript
+
+Variáveis: Armazenam valores para uso posterior.
+- let: Pode ser reatribuído.
+- const: Não pode ser reatribuído.
+*/
+
+/*
+Nomenclatura:
+- Use CamelCase para variáveis com múltiplas palavras.
+- Pode conter letras, números, _ ou $, mas não pode começar com número.
+*/
+
+/*
+Boa Prática:
+- let para valores mutáveis e const para valores constantes.
+*/
+
+/*
+Comentários:
+
+Linha: // comentário
+Multilinha: /* comentário * /
+Atalho: Ctrl + ; para comentar rapidamente.
+*/
