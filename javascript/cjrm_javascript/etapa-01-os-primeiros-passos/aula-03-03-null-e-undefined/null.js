b8491dd0ad7aea856f3d6f4460bb3e9fb2383cdd
Raíza Cirne <raizacirne@gmail.com>
2025-04-16 21:04:12 -0300

    feat: explore undefined and null behaviors

---
+++ javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-03-03-null-e-undefined/null.js
@@ -0,0 +1,9 @@
+let emptiness = null;
+
+console.log(emptiness, emptiness + 3, `o valor é ${emptiness}`);
+
+// emptiness -> null 
+
+// emptiness + 3 -> 3 -> Execução matemática que envolve null é interpretado automaticamente como zero. 
+
+// `o valor é ${emptiness}` -> null é convertido para string 
---
+++ javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-03-03-null-e-undefined/undefined.js
@@ -0,0 +1,9 @@
+let emptiness;
+
+console.log(emptiness, emptiness + 3, `o valor é ${emptiness}`);
+
+// emptiness -> undefined -> ainda não atribuímos valor pra ela. 
+
+// empetiness + 3 -> NaN -> undefined em uma expressão numérica retorna NaN 
+
+// `o valor é ${emptiness}` -> undefined -> dentro de string é convertido para string 
