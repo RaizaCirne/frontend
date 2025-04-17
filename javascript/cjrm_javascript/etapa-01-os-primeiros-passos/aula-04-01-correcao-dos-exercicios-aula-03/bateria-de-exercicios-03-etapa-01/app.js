8b8ce7887d150ff3668a007918291d7157818688
Raíza Cirne <raizacirne@gmail.com>
2025-04-16 21:10:22 -0300

    add exercise battery

--- javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-04-01-correcao-dos-exercicios-aula-03/bateria-de-exercicios-03-etapa-01/app.js
+++ javascript/cjrm_javascript/etapa-01-os-primeiros-passos/aula-04-01-correcao-dos-exercicios-aula-03/bateria-de-exercicios-03-etapa-01/app.js
@@ -10,7 +10,7 @@
 02 - Armazene um array vazio em uma let "brazilianFoods";
 */
 
-let brazilianFoods = [];
+
 
 /*
 03 - Use o método adequado para adicionar os 3 seguintes itens ao array:
@@ -22,8 +22,7 @@ let brazilianFoods = [];
 - Exiba a brazilianFoods no console.
 */
 
-brazilianFoods.push("Coxinha", null, "Brigadeiro");
-// console.log(brazilianFoods);
+
 
 /*
 04 - Comente o console.log acima e:
@@ -33,8 +32,7 @@ array "brazilianFoods" (null) seja substituído por "Pão de Queijo";
 - Exiba apenas o 2º item desse array no console.
 */
 
-brazilianFoods[1] = "Pão de Queijo";
-// console.log(brazilianFoods[1]);
+
 
 /*
 05 - Comente o console.log acima e:
@@ -49,9 +47,7 @@ mas sem digitar os nomes das comidas diretamente;
 - Exiba a foodsInfo no console.
 */
 
-const foodsInfo = `Até aqui, o "brazilianFoods" possui ${brazilianFoods.length} itens: ${brazilianFoods[0]}, ${brazilianFoods[1]} e  ${brazilianFoods[2]} `;
-// console.log(foodsInfo);
-// OBS: Dá mesma forma que a length traz o comprimento de uma string, ela traz a quantidade de intens que um array tem.
+
 
 /*
 06 - Comente o console.log acima e:
@@ -65,10 +61,7 @@ itens "Lilica" e "Matilda";
 - Exiba a "dogNames" no console.
 */
 
-const maleDogNames = ["Chico", "Zeca"];
-const femaleDogNames = ["Lilica", "Matilda"];
-let dogNames = maleDogNames.concat(femaleDogNames);
-// console.log(dogNames);
+
 
 /*
 07 - Comente o console.log acima e:
@@ -79,7 +72,7 @@ let dogNames = maleDogNames.concat(femaleDogNames);
 - O resultado deve ser: "nome1, nome2, nome3, nome4".
 */
 
-// console.log(dogNames.join(", "));
+
 
 /*
 08 - Comente o console.log acima e:
@@ -94,10 +87,7 @@ utilizando o método adequado para isso;
 agora é "Lilica".
 */
 
-dogNames.pop();
-// console.log(dogNames[dogNames.length - 1]);
-// dogNames.length - 1 para obter o útimo item do array
-//
+
 
 /*
 09 - Comente o console.log acima e:
@@ -111,10 +101,7 @@ apenas as letras iniciais dos nomes do array "dogNames";
 - Exiba a "initials" no console.
 */
 
-dogNames.push("Nina");
-const initials = `${dogNames[0][0]}${dogNames[1][0]}${dogNames[2][0]}${dogNames[3][0]}`;
-// Existem duas formas de obter só a primeira letra: Com slice ou apenas notação de colchetes. Nesse caso usamos a notação de colchetes para escrever menos. ${dogNames[acessa item do array][acessa caractere desse item]}
-// console.log(initials);
+
 
 /*
 10 - Comente o console.log acima e:
@@ -123,7 +110,7 @@ const initials = `${dogNames[0][0]}${dogNames[1][0]}${dogNames[2][0]}${dogNames[
 - Utilize o método adequado para isso.
 */
 
-// console.log(initials.toLowerCase());
+
 
 /*
 11 - Comente o console.log acima e:
@@ -141,12 +128,7 @@ constante que você criou.
 'A SOBREMESA é um doce à base de coco, tradicional na América Latina e em Angola.'
 */
 
-const dessert = `${dogNames[0].slice(3)}${dogNames[1].slice(
-  2
-)}${dogNames[3].slice(2)}`.replace("n", "d");
-// console.log(`A ${dessert} é um doce a base de coco, tradicional da América Latina e Angola.`);
-// ${dogNames[primeiro item do array].slice(index do caractere a partir do qual a parte da string deve ser pega)}
-// ${dogNames[0].slice(3)} -> Quando não passamos o segundo argumento, o JS entende que a partir do index 3 quero pegar todo o restante da string
+
 
 /*
 12 - Comente o console.log acima e:
@@ -160,7 +142,3 @@ elevado ao cubo, faça o 2º item do array receber todo o valor que ele
 já tem, mais 4;
 - Agora, o resultado exibido no console deve ser 729.
 */
-
-let oddNumbers = [3, 5, 7];
-oddNumbers[1] += 4;
-console.log(oddNumbers[1] ** 3);
