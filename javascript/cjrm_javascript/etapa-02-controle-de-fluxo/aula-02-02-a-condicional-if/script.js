14b6d6798c0d4122746fa506c833d6740b1b55e9
Raíza Cirne <raizacirne@gmail.com>
2025-07-31 16:12:32 -0300

    chore: add code example

---
+++ javascript/cjrm_javascript/etapa-02-controle-de-fluxo/aula-02-02-a-condicional-if/script.js
@@ -0,0 +1,12 @@
+// if
+const age = 19;
+
+if (age > 18) {
+  console.log("Você tem mais de 18 anos");
+}
+
+const simpsons = ["Marge", "Homer", "Lisa", "Bart"];
+
+if (simpsons.length >= 3) {
+  console.log("o array tem bastante personagens");
+}
