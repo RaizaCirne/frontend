9298ac194233c634707c303c3fb97c01609cd179
Raíza Cirne <raizacirne@gmail.com>
2025-12-01 17:58:51 -0300

    feat: add basic variable initialization and calculation logic

---
+++ javascript/bia-codes/Aula02-Declarando-variaveis/declaringVariables.js
@@ -0,0 +1,14 @@
+// let const
+
+const primeiroValor = 10;
+const segundoValor = 20;
+
+console.log("PrimeiroValor + segundoValor:", primeiroValor + segundoValor);
+const valorTotal = primeiroValor + segundoValor;
+console.log("valorTotal: ", valorTotal);
+
+let terceiroValor = 50;
+console.log(terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValor final: ", terceiroValor);
