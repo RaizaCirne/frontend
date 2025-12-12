3df48aec94ee7bfa488634a3f2f705c5924f9710
Raíza Cirne <raizacirne@gmail.com>
2025-12-12 20:22:54 -0300

    feat: implement utility and foundational JavaScript functions

---
+++ javascript/origamid_javascript/02-javascript-para-iniciantes/0205-funcoes/exercicio.js
@@ -0,0 +1,68 @@
+// Crie uma função para verificar se um valor é Truthy
+
+function isTruth(dado) {
+  return !!dado;
+}
+
+console.log(isTruth("s"));
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+function perimetroQuadrado(lado) {
+  return lado * 4;
+}
+
+console.log(perimetroQuadrado(3));
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
+
+console.log(nomeCompleto("Raíza", "Cirne"));
+
+// Crie uma função que verifica se um número é par
+
+function isEven(numero) {
+  var modulo = numero % 2;
+  if (modulo === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isEven(10));
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+function tipoDeDado(dado) {
+  return typeof dado;
+}
+
+console.log(tipoDeDado(10));
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'click' ocorrer.
+
+addEventListener("click", function () {
+  console.log("Raíza Cirne");
+});
+
+// Corrija o erro abaixo
+var totalPaises = 193;
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+
+console.log(precisoVisitar(20));
+console.log(jaVisitei(20));
