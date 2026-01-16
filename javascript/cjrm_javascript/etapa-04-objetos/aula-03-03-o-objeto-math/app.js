929b803aa88a35e160e6e90c57b90b282abc495a
Raíza Cirne <raizacirne@gmail.com>
2026-01-16 13:40:56 -0300

    feat(math): implement examples of Math object and its methods

---
+++ javascript/cjrm_javascript/etapa-04-objetos/aula-03-03-o-objeto-math/app.js
@@ -0,0 +1,30 @@
+// o objeto Math
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area)); // 8
+console.log(Math.floor(area)); // 7
+console.log(Math.floor(area)); // 7
+console.log(Math.ceil(area)); // 8
+console.log(Math.trunc(area)); // 7
+
+// numeros aleatórios
+const randomNumber = Math.random(); 
+
+console.log(randomNumber); // gera número aleatório entre 0 e 1
+console.log(Math.round(randomNumber)); // 1 ou 0 é exibido no console
+console.log(Math.round(randomNumber * 100)); // gera número aleatório entre 0 e 100
+
+// -------------------------------------------------------------------------------------
+
+// round() - converte um número decimal em inteiro da forma padrão: se o decimal for < .5 o número é arredondado para baixo, se for > .5, é arredondado para cima
+
+// floor() - sempre arredonda o número decimal para baixo
+
+// ceil() - sempre arredonda o número decimal para cima
+
+// trunc() - remove o decimal do número recebido por argumento e retorna a parte inteira dele.
