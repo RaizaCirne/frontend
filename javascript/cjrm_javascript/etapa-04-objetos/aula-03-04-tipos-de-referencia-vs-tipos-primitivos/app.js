65d64e438f4bf2db737979169f206f54e57d760f
Raíza Cirne <raizacirne@gmail.com>
2026-01-17 15:46:00 -0300

    doc(types): add explanations for primitive and reference values

--- javascript/cjrm_javascript/etapa-04-objetos/aula-03-04-tipos-de-referencia-vs-tipos-primitivos/app.js
+++ javascript/cjrm_javascript/etapa-04-objetos/aula-03-04-tipos-de-referencia-vs-tipos-primitivos/app.js
@@ -1,18 +1,18 @@
 // valores primitivos
 let scoreOne = 50;
-let scoreTwo = scoreOne;
+let scoreTwo = scoreOne; // Uma cópia desse valor é criada e armazenada na memória stack, pois estamos lidando com valor primitivo.
 
-console.log(`scoreOne: ${scoreOne} | scoreTwo: ${scoreTwo}`);
+console.log(`scoreOne: ${scoreOne} | scoreTwo: ${scoreTwo}`); // Tanto a scoreOne quanto a scoreTwo armazenam 50
 
-scoreOne = 100;
-console.log(`scoreOne: ${scoreOne} | scoreTwo: ${scoreTwo}`);
+scoreOne = 100; // Modificamos o valor da scoreOne
+console.log(`scoreOne: ${scoreOne} | scoreTwo: ${scoreTwo}`); // Agora a scoreOne armazena 100 e a scoreTwo continua armazenando 50 
 
 // valores de referência
 let userOne = { name: "Roger", age: 31 };
-let userTwo = userOne;
+let userTwo = userOne; // Tentamos criar uma cópia do objeto userOne que está armazenado na memória heap. As variáveis userOne e userTwo estão apontando para o mesmo objeto -> { name: "Roger", age: 31 }
 
-console.log(userOne, userTwo);
+console.log(userOne, userTwo); 
 
-userOne.age = 32;
+userOne.age = 32; // Modificando o valor da age que userOne armazena 
 
-console.log(userOne, userTwo);
+console.log(userOne, userTwo); // O valor da age dos dois objetos foram modificadas para 32. Visualizamos o mesmo objeto, pois userOne e userTwo apontam para o mesmo objeto no heap. 
