86981bf12531dcb790d0e6c24907f27f076e2006
Raíza Cirne <raizacirne@gmail.com>
2026-01-22 17:42:17 -0300

    feat(dom): implement window and dom selection examples

---
+++ javascript/origamid_javascript/03-dom-para-iniciantes/0301-o-que-e-o-dom/script-estudo.js
@@ -0,0 +1,12 @@
+// window.alert("Isso mesmo");
+
+const href = window.location.href;
+console.log(href);
+
+if (href === "http://127.0.0.1:5500/index.html") {
+  console.log("É igual");
+}
+
+const h1Selecionado = document.querySelector("h1");
+
+alert("Teste");
