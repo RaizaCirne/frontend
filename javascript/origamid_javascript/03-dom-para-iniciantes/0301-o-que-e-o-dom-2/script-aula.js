e0ea0eb2dddb0dc3c6f411ab9bbf30f0d5ec97c4
Raíza Cirne <raizacirne@gmail.com>
2026-01-24 16:59:02 -0300

    feat(dom): solve window and document property exercises

--- javascript/origamid_javascript/03-dom-para-iniciantes/0301-o-que-e-o-dom-2/script-aula.js
+++ javascript/origamid_javascript/03-dom-para-iniciantes/0301-o-que-e-o-dom-2/script-aula.js
@@ -4,15 +4,15 @@ const href = window.location.href;
 
 console.log(href);
 
-if(href === 'http://127.0.0.1:5500/o-que-e-o-doms/') {
-  console.log('É igual');
+if (href === "http://127.0.0.1:5500/o-que-e-o-doms/") {
+  console.log("É igual");
 }
 
-const h1Selecionado = document.querySelector('h1');
+const h1Selecionado = document.querySelector("h1");
 const h1Classes = h1Selecionado.classList;
 
 function teste() {
-  console.log('Clicou em ', h1Selecionado.innerText);
+  console.log("Clicou em ", h1Selecionado.innerText);
 }
 
-h1Selecionado.addEventListener('click', teste);
+h1Selecionado.addEventListener("click", teste);
---
+++ javascript/origamid_javascript/03-dom-para-iniciantes/0301-o-que-e-o-dom-2/script-exercicio.js
@@ -0,0 +1,20 @@
+// Retorne o url da página atual utilizando o objeto window
+
+const hrefPagina = window.location.href;
+console.log(hrefPagina);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const elementoAtivo = document.querySelector(".ativo");
+console.log(elementoAtivo);
+
+// Retorne a linguagem do navegador
+
+const linguagemNavegador = window.navigator.language;
+console.log(linguagemNavegador);
+
+// Retorne a largura da janela
+
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
